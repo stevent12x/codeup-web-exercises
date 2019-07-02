dc3a91546b5a226fa463338e46184fba21926c89
Steven Turner <steven.turner12x@gmail.com>
2019-07-02 15:01:36 -0500

    mouse events in jquery

--- js/jquery_exercises_external.js
+++ js/jquery_exercises_external.js
@@ -2,9 +2,28 @@
 
 // var contents = $('#morbo').html();
 //
-
+//
 // $('.codeup').css('border', '1px solid red');
+//
+// $('li').css('font-size', '20px');
+// $('h1, p, li').css('background-color', 'lightpink');
+// alert($('h1').html());
 
-$('li').css('font-size', '20px');
-$('h1, p, li').css('background-color', 'yellow');
-alert($('h1').html());
+$().ready(
+	function () {
+		$('h1').click(function () {
+			$(this).css('background-color', 'lightyellow');
+		});
+		$('p').dblclick(function () {
+			$(this).css('font-size', '18px');
+		});
+		$('li').hover(
+			function () {
+				$(this).css('color', 'firebrick');
+		},
+			function () {
+				$(this).css('color', 'black')
+			}
+		);
+	}
+)
