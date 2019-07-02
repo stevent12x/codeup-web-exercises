a6a2cba5d8c46c64498bf37f6bec372994279ed7
Steven Turner <steven.turner12x@gmail.com>
2019-07-02 12:05:30 -0500

    jquery element selectors

--- js/jquery_exercises_external.js
+++ js/jquery_exercises_external.js
@@ -3,4 +3,8 @@
 // var contents = $('#morbo').html();
 //
 
-$('.codeup').css('border', '1px solid red');
+// $('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px');
+$('h1, p, li').css('background-color', 'yellow');
+alert($('h1').html());
