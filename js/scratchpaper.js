b349b03a54516d9095cc4dc37888e6364af6cbde
Steven Turner <steven.turner12x@gmail.com>
2019-07-02 11:04:42 -0500

    scratch for 2nd assessment

---
+++ js/scratchpaper.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var dogsArray = [
+	{
+		name: "Chompers",
+		breed: "Pug",
+		age: 7
+	},
+	{
+		name: "Freddy",
+		breed: "Lab",
+		age: 4
+	},
+	{
+		name: "Mr. Pig",
+		breed: "Mastif",
+		age: 10
+	}
+]
+
+
+\
