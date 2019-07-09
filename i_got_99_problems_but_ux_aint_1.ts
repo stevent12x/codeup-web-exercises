6399f328238f912f0d1632d48cb86fc36fc7c814
Steven Turner <steven.turner12x@gmail.com>
2019-07-09 12:28:21 -0500

    personal website first commit

---
+++ i_got_99_problems_but_ux_aint_1.ts
@@ -0,0 +1,15 @@
+//
+//
+// I Got 99 Problems But UX Ain't One
+//
+//
+// 1.) Issue: Horrible restaurant POS (point-of-sale) systems that cause frustration rather than streamline the process of placing orders.
+//     Solution: An app that allows for easy BOH communication, customizable page organization and layout, and OTF modification of CC payments.
+//
+//
+// 2.) Issue: San Antonio's VIA transport app.
+//     Solution: A mobile app paired with onboard GPS systems that update regularly and properly communicate information to the user.
+//
+//
+// 3.) Issue: Birds aren't real.
+//     Solution: A mobile app that allows the user to take pictures of any and all "birds" in the area and automatically reports them to the proper authorities.
