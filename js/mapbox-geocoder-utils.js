914eea3936777d1ca97605adeb5d15485ed0b27b
Steven Turner <steven.turner12x@gmail.com>
2019-07-14 12:10:29 -0500

    7/14 good morning commit

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -21,7 +21,8 @@ function geocode(search, token) {
 			return res.json();
 			// to get all the data from the request, comment out the following three lines...
 		}).then(function(data) {
-			return data.features[0].center;
+			return data.features[0];
+			console.log(data.features[0])
 		});
 }
 
