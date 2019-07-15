fc8b1b7bea0912d3b120bfe6546ea42ad0524583
Steven Turner <steven.turner12x@gmail.com>
2019-07-15 15:33:50 -0500

    weather app 7/14

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -25,18 +25,17 @@ function geocode(search, token) {
 			console.log(data.features[0])
 		});
 }
-
-	function reverseGeocode(coordinates, token) {
-		var baseUrl = 'https://api.mapbox.com';
-		var endPoint = '/geocoding/v5/mapbox.places/';
-		return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
-			.then(function (res) {
-				return res.json();
-			})
-			// to get all the data from the request, comment out the following three lines...
-			.then(function (data) {
-				return data.features[0].place_name;
-			});
-	}
-
+	//
+	// function reverseGeocode(coordinates, token) {
+	// 	var baseUrl = 'https://api.mapbox.com';
+	// 	var endPoint = '/geocoding/v5/mapbox.places/';
+	// 	return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+	// 		.then(function (res) {
+	// 			return res.json();
+	// 		})
+	// 		// to get all the data from the request, comment out the following three lines...
+	// 		.then(function (data) {
+	// 			return data.features[0].place_name;
+	// 		});
+	// }
 
---
+++ js/weather_call_icons.js
@@ -0,0 +1,12 @@
+var weatherIcon = [{
+	clearDay: url("./img/climacons - master / SVG / Sun.svg"),
+	clearNight: url("./img/climacons-master/SVG/Moon-Full.svg"),
+	rain: url("./img/climacons-master/SVG/Cloud-Rain.svg"),
+	snow: url("./img/climacons-master/SVG/Cloud-Snow.svg"),
+	sleet: url("./img/climacons-master/SVG/Cloud-Hail.svg"),
+	wind: url("./img/climacons-master/SVG/Wind.svg"),
+	fog: url("./img/climacons-master/SVG/Cloud-Fog.svg"),
+	cloudy: url("./img/climacons-master/SVG/Cloud.svg"),
+	partlyCloudyDay: url("./img/climacons-master/SVG/Cloud-Sun.svg"),
+	partlyCloudyNight: url("./img/climacons-master/SVG/Cloud-Moon.svg")
+}];
