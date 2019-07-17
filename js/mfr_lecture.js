f29c4bc678e8f62870ab703aa1f9aa83328f7b1d
Steven <steven.turner12x@gmail.com>
2019-07-17 11:56:50 -0500

    .reduce exercise

--- js/mfr_lecture.js
+++ js/mfr_lecture.js
@@ -19,7 +19,7 @@ const books = [
         genres: ["dystopian","sci-fi","political fiction"],
         yearsInPublication: 70
     },
-
+    {
         title: "Noruwei no Mori",
         author: "Haruki Murakami",
         genres: ["coming of age", "novel"],
@@ -48,3 +48,42 @@ const threeGenres = books.filter(book => book.genres.length > 2)
     REDUCE
     using .reduce, return the total years between all books.
  */
+
+// const totalYears = books.yearsInPublication.reduce(function (currentSum, currentBook) {
+//     return currentSum + currentBook
+// },0);
+
+const sumOfYears = books.reduce(function (accumulation, current) {
+    const currentBook = current.yearsInPublication
+    return accumulation + currentBook
+},0)
+
+console.log(sumOfYears)
+
+const longestName = books.reduce(function (previous, current) {
+    const  currentName = current.author;
+    if (currentName.length > previous.length){
+        return currentName
+    } else {
+        return previous
+    }
+},"");
+console.log(longestName)
+
+
+
+
+
+// function countWords(sentence) {
+//     const words = sentence.split(" ");
+//
+//     const wordCountObject = words.reduce((wordCounts, word) => {
+//         if (typeof word == 'undefined') {
+//             return wordCounts + 1
+//         } else {
+//             return wordCounts += 1
+//         }
+//     },{})
+//
+//     console.log(countWords("The depths of mankind are filled with toil, sadness, and woe. Bust it down Thotiana. Bring it back now."))
+// }
