c944e07e329ba433959526719c009f720e7ff467
Steven Turner <steven.turner12x@gmail.com>
2019-07-26 13:54:12 -0500

    "7/26 commit"

---
+++ js/scratchpaper.js
@@ -0,0 +1,11 @@
+"use strict";
+
+if (true) {
+	let x = 5;
+}
+console.log(typeof x);
+
+const sayHello = (name) => {'hello' + name}
+
+let num = [1,2,3,4,5]
+num.map(n => n*3)
--- js/warmup.js
+++ js/warmup.js
@@ -1,6 +1,21 @@
 
 
-const goodMorning = (greeting = 'Good morning', cohort = 'Betelgeuse') => {return `${greeting} ${cohort}!`};
+// const goodMorning = (greeting = 'Good morning', cohort = 'Betelgeuse') => {return `${greeting} ${cohort}!`};
+// // // //
+// // // // console.log(goodMorning());
+// // // // console.log(goodMorning(undefined, "Ceres"));
 
-console.log(goodMorning());
-console.log(goodMorning(undefined, "Ceres"));
+(function () {
+
+	let timer = 0;
+
+		setInterval(function () {
+			console.log(timer++*10)
+		}, 10);
+
+		const url = "http://www.omdbapi.com/?apikey=" + movieKey + "&t=batman";
+		fetch(url).then(() => {
+			console.log('ding');
+		clearInterval(setInterval)
+		});
+})();
